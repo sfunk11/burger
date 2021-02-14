81c3be70156848d3513cf0802247c43f2c978d12
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-02-14 14:57:56 -0500

    fixed insert and update scripts / views

---
+++ public/assets/js/index.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burger").val().trim()
+      };
+  console.log(newBurger)
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".eat").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEaten = $(this).data("neweaten") === false;
+  
+      var newEatenState = {
+        devoured:  newEaten
+      };
+      console.log('id:' + id)
+      console.log('eaten:' + newEatenState.devoured)
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed eaten state to", newEaten);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  })
